3a5fd93c33ae38b3f072c46f44ae8655005012e2
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2025-03-02 16:48:45 +0300

    move to cpp file

---
+++ opencl_wrapper.cpp
@@ -0,0 +1,168 @@
+#include "opencl_wrapper.h"
+
+std::string ocl::to_error_string(cl_int err) {
+    switch (err) {
+        case CL_SUCCESS: return "Success";
+        case CL_DEVICE_NOT_FOUND: return "Device not found";
+        case CL_DEVICE_NOT_AVAILABLE: return "Device not available";
+        case CL_COMPILER_NOT_AVAILABLE: return "Compiler not available";
+        case CL_MEM_OBJECT_ALLOCATION_FAILURE: return "Memory object allocation failure";
+        case CL_OUT_OF_RESOURCES: return "Out of resources";
+        case CL_OUT_OF_HOST_MEMORY: return "Out of host memory";
+        case CL_PROFILING_INFO_NOT_AVAILABLE: return "Profiling information not available";
+        case CL_MEM_COPY_OVERLAP: return "Memory copy overlap";
+        case CL_IMAGE_FORMAT_MISMATCH: return "Image format mismatch";
+        case CL_IMAGE_FORMAT_NOT_SUPPORTED: return "Image format not supported";
+        case CL_BUILD_PROGRAM_FAILURE: return "Program build failure";
+        case CL_MAP_FAILURE: return "Map failure";
+
+        default: {
+            return "Unknown OpenCL error: " + std::to_string(err);
+        }
+    }
+}
+
+std::runtime_error ocl::cl_error(cl_int err) {
+    return std::runtime_error("OpenCL error: " + to_error_string(err));
+}
+
+ocl::proque::~proque() {
+    if (kernel) {
+        clReleaseKernel(kernel);
+    }
+    if (program) {
+        clReleaseProgram(program);
+    }
+    if (queue) {
+        clReleaseCommandQueue(queue);
+    }
+    if (context) {
+        clReleaseContext(context);
+    }
+}
+
+ocl::proque::proque(const char *source, cl_queue_flags flags) {
+    cl_int err;
+    cl_platform_id platform;
+    err = clGetPlatformIDs(1, &platform, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+
+    context = clCreateContextFromType(
+        (cl_context_properties[]){CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0},
+        CL_DEVICE_TYPE_GPU,
+        nullptr, nullptr, &err);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+
+    cl_device_id device;
+    err = clGetContextInfo(context, CL_CONTEXT_DEVICES, sizeof(device), &device, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+
+    queue = clCreateCommandQueue(context, device, flags, &err);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+
+    program = clCreateProgramWithSource(context, 1, &source, nullptr, &err);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+
+    err = clBuildProgram(program, 0, nullptr, nullptr, nullptr, nullptr);
+    if (err != CL_SUCCESS) {
+        size_t log_size;
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &log_size);
+        std::string log(log_size, '\0');
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, &log[0], nullptr);
+        throw std::runtime_error("Failed to build OpenCL program:\n" + log);
+    }
+}
+
+cl_mem ocl::proque::create_buffer(size_t size, cl_mem_flags flags, void *data) {
+    cl_int err;
+    cl_mem buffer = clCreateBuffer(context, flags, size, data, &err);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+    return buffer;
+}
+
+void ocl::proque::create_kernel(const char *name) {
+    cl_int err;
+    kernel = clCreateKernel(program, name, &err);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+}
+
+void ocl::proque::set_arg(cl_uint index, const void *value, size_t size) {
+    // maybe add check kernel != nullptr
+    cl_int err = clSetKernelArg(kernel, index, size, value);
+    if (err != CL_SUCCESS) {
+        throw std::runtime_error("Failed to set OpenCL kernel argument");
+    }
+}
+
+void ocl::proque::set_arg(cl_uint index, const void *value) {
+    set_arg(index, value, sizeof(value));
+}
+
+void ocl::proque::run_kernel(size_t work_size, size_t local_work_size) {
+    const size_t* lws = (local_work_size != 0 ? &local_work_size : nullptr);
+    cl_int err = clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &work_size,
+        lws, 0, nullptr, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+}
+
+double ocl::proque::run_kernel_with_profiling(size_t work_size, size_t local_work_size) {
+    const size_t* lws = (local_work_size != 0 ? &local_work_size : nullptr);
+    cl_event kernel_event;
+    cl_int err = clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &work_size, lws, 0, nullptr, &kernel_event);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+
+    finish();
+
+    cl_ulong start_time, end_time;
+
+    err = clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_START, sizeof(start_time), &start_time, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+    err = clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_END, sizeof(end_time), &end_time, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+    clReleaseEvent(kernel_event);
+    double execution_time = (end_time - start_time) * 1e-6;
+    return execution_time;
+}
+
+void ocl::proque::finish() {
+    cl_int err = clFinish(queue);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+}
+
+void ocl::proque::read_buffer(cl_mem buffer, size_t size, void *ptr) {
+    cl_int err = clEnqueueReadBuffer(queue, buffer, CL_TRUE, 0, size, ptr, 0, nullptr, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+}
+
+void ocl::proque::write_buffer(cl_mem buffer, size_t size, const void *ptr) {
+    cl_int err = clEnqueueWriteBuffer(queue, buffer, CL_TRUE, 0, size, ptr, 0, nullptr, nullptr);
+    if (err != CL_SUCCESS) {
+        throw cl_error(err);
+    }
+}
--- opencl_wrapper.h
+++ opencl_wrapper.h
@@ -1,34 +1,14 @@
+#pragma once
+
 #include <OpenCL/opencl.h>
 #include <stdexcept>
 #include <iostream>
 
 namespace ocl {
 
-    inline std::string to_error_string(cl_int err) {
-        switch (err) {
-            case CL_SUCCESS: return "Success";
-            case CL_DEVICE_NOT_FOUND: return "Device not found";
-            case CL_DEVICE_NOT_AVAILABLE: return "Device not available";
-            case CL_COMPILER_NOT_AVAILABLE: return "Compiler not available";
-            case CL_MEM_OBJECT_ALLOCATION_FAILURE: return "Memory object allocation failure";
-            case CL_OUT_OF_RESOURCES: return "Out of resources";
-            case CL_OUT_OF_HOST_MEMORY: return "Out of host memory";
-            case CL_PROFILING_INFO_NOT_AVAILABLE: return "Profiling information not available";
-            case CL_MEM_COPY_OVERLAP: return "Memory copy overlap";
-            case CL_IMAGE_FORMAT_MISMATCH: return "Image format mismatch";
-            case CL_IMAGE_FORMAT_NOT_SUPPORTED: return "Image format not supported";
-            case CL_BUILD_PROGRAM_FAILURE: return "Program build failure";
-            case CL_MAP_FAILURE: return "Map failure";
+    inline std::string to_error_string(cl_int err);
 
-            default: {
-                return "Unknown OpenCL error: " + std::to_string(err);
-            }
-        }
-    }
-
-    inline std::runtime_error cl_error(cl_int err) {
-        return std::runtime_error("OpenCL error: " + to_error_string(err));
-    }
+    inline std::runtime_error cl_error(cl_int err);
 
     struct proque {
         cl_context context = nullptr;
@@ -37,148 +17,26 @@ namespace ocl {
         cl_kernel kernel = nullptr;
         cl_device_id device = nullptr;
 
-        ~proque() {
-            if (kernel) {
-                clReleaseKernel(kernel);
-            }
-            if (program) {
-                clReleaseProgram(program);
-            }
-            if (queue) {
-                clReleaseCommandQueue(queue);
-            }
-            if (context) {
-                clReleaseContext(context);
-            }
-        }
+        ~proque();
 
+        proque(const char *source, cl_queue_flags flags = 0);
 
-        proque(const char *source, cl_queue_flags flags = 0) {
-            cl_int err;
-            cl_platform_id platform;
-            err = clGetPlatformIDs(1, &platform, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
+        cl_mem create_buffer(size_t size, cl_mem_flags flags, void* data);
 
-            context = clCreateContextFromType(
-                (cl_context_properties[]){CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0},
-                CL_DEVICE_TYPE_GPU,
-                nullptr, nullptr, &err);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
+        void create_kernel(const char *name);
 
-            cl_device_id device;
-            err = clGetContextInfo(context, CL_CONTEXT_DEVICES, sizeof(device), &device, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
+        void set_arg(cl_uint index, const void *value, size_t size);
 
-            queue = clCreateCommandQueue(context, device, flags, &err);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
+        void set_arg(cl_uint index, const void* value);
 
-            program = clCreateProgramWithSource(context, 1, &source, nullptr, &err);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
+        void run_kernel(size_t work_size, size_t local_work_size = 0);
 
-            err = clBuildProgram(program, 0, nullptr, nullptr, nullptr, nullptr);
-            if (err != CL_SUCCESS) {
-                size_t log_size;
-                clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &log_size);
-                std::string log(log_size, '\0');
-                clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, &log[0], nullptr);
-                throw std::runtime_error("Failed to build OpenCL program:\n" + log);
-            }
-        }
+        double run_kernel_with_profiling(size_t work_size, size_t local_work_size = 0);
 
-        cl_mem create_buffer(size_t size, cl_mem_flags flags, void* data) {
-            cl_int err;
-            cl_mem buffer = clCreateBuffer(context, flags, size, data, &err);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-            return buffer;
-        }
+        void finish();
 
-        void create_kernel(const char *name) {
-            cl_int err;
-            kernel = clCreateKernel(program, name, &err);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-        }
+        void read_buffer(cl_mem buffer, size_t size, void* ptr);
 
-        void set_arg(cl_uint index, const void *value, size_t size) {
-            // maybe add check kernel != nullptr
-            cl_int err = clSetKernelArg(kernel, index, size, value);
-            if (err != CL_SUCCESS) {
-                throw std::runtime_error("Failed to set OpenCL kernel argument");
-            }
-        }
-
-        void set_arg(cl_uint index, const void* value) {
-            set_arg(index, value, sizeof(value));
-        }
-
-        void run_kernel(size_t work_size, size_t local_work_size = 0) {
-            const size_t* lws = (local_work_size != 0 ? &local_work_size : nullptr);
-            cl_int err = clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &work_size,
-                lws, 0, nullptr, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-        }
-
-        double run_kernel_with_profiling(size_t work_size, size_t local_work_size = 0) {
-            const size_t* lws = (local_work_size != 0 ? &local_work_size : nullptr);
-            cl_event kernel_event;
-            cl_int err = clEnqueueNDRangeKernel(queue, kernel, 1, nullptr, &work_size, lws, 0, nullptr, &kernel_event);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-
-            finish();
-
-            cl_ulong start_time, end_time;
-
-            err = clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_START, sizeof(start_time), &start_time, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-            err = clGetEventProfilingInfo(kernel_event, CL_PROFILING_COMMAND_END, sizeof(end_time), &end_time, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-            clReleaseEvent(kernel_event);
-            double execution_time = (end_time - start_time) * 1e-6;
-            return execution_time;
-
-        }
-
-        void finish() {
-            cl_int err = clFinish(queue);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-        }
-
-        void read_buffer(cl_mem buffer, size_t size, void* ptr) {
-            cl_int err = clEnqueueReadBuffer(queue, buffer, CL_TRUE, 0, size, ptr, 0, nullptr, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-        }
-
-        void write_buffer(cl_mem buffer, size_t size, const void* ptr) {
-            cl_int err = clEnqueueWriteBuffer(queue, buffer, CL_TRUE, 0, size, ptr, 0, nullptr, nullptr);
-            if (err != CL_SUCCESS) {
-                throw cl_error(err);
-            }
-        }
-
+        void write_buffer(cl_mem buffer, size_t size, const void* ptr);
     };
 }
